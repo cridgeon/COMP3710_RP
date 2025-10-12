d2363eb80ff6dcf042cee1eb025399b5452209e8
cridgeon <charlie@ridgeon.com.au>
2025-10-12 22:18:53 +1000

    FEAT: updated data loading with hyperparameters, anchors and normalisation

--- recognition/siamese_kaggle_48020774/README.md
+++ recognition/siamese_kaggle_48020774/README.md
@@ -9,6 +9,41 @@ Ensure you have [Miniconda](https://www.anaconda.com/docs/getting-started/minico
 If not otherwise specified it is assumed that the working directory that commands are 
 executed in is the `recognition/siamese_kaggle_48020774` directory.
 
+<h2> Model and Training Breakdown </h2>
+
+<h3> Data </h3>
+
+The main concern while using the dataset was the large disparity between the number of 
+positive (malignant) and negative (benign) classifications. For the entire dataset, 
+only 0.018% of the images have a malignant classification. This large disparity would be 
+certain to cause issues during training, as there is a heavy bias towards a model that 
+classifies all images as benign. Thus a hyperparameter defining the desired split of 
+positive and negative classfications was added and con be configured in 
+`utility/config.json`, by changing the value of the `class_split` parameter.
+
+Note that this parameter is the main one used for determining the number of images loaded
+into memory. Since there are more negative classifications, a lower percentage will allow
+for more of the data to be used for training, while a higher percentage means that less
+data can be used in order to maintain the correct ratio.
+
+<h3> hyperparameters </h3>
+
+Below is a list of hyperparameters and their functions
+
+| Name | Function |
+| ---- | -------- |
+| `data_dir`         | Directory where the dataset is stored relative to working directory|
+| `train_split`      | Fraction of data used for training (rest for validation/testing).  |
+| `class_split`      | Proportion of positive (malignant) samples in the dataset.   |
+| `max_images   `    | The maximum number of images loaded into mem. To prevent mem errors|
+| `anchor_count`     | The number of images to reserve for use as anchors.    |
+| `image_width`      | Width to resize images to during preprocessing.        |
+| `image_height`     | Height to resize images to during preprocessing.       |
+| `batch_size`       | Number of samples per training batch.                  |
+| `epochs`           | Number of training epochs.                             |
+| `learning_rate`    | Learning rate for the optimizer.                       |
+| `model_save_path`  | File path to save the trained model.                   |
+
 <h2> Using the model </h2>
 
 <h3> Download the dataset </h3>
@@ -39,6 +74,14 @@ executed in is the `recognition/siamese_kaggle_48020774` directory.
   ```
 
 <h3> Setting up the conda environment </h3>
-- run the conda_setup script
+Run the conda_setup script:
 
-  `./utility/conda_setup.sh`
+```    
+./utility/conda_setup.sh
+```
+
+Activate the environment:
+
+```
+conda activate siamese-48020774
+```
--- recognition/siamese_kaggle_48020774/dataset.py
+++ recognition/siamese_kaggle_48020774/dataset.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -8,7 +9,7 @@ import json
 def list_ISIC_images(folder):
     return sorted([f.split(".")[0] for f in os.listdir(folder) if f.endswith(".jpg")])
 
-def load_ISIC_tensors(data_dir: str, num_images: int, split: float, w: int = 3000, h: int = 2000) -> ((tf.Tensor, tf.Tensor), (tf.Tensor, tf.Tensor)):
+def load_ISIC_tensors() -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
     """
     Loads all images from a folder into a single 4-D tenso.
     
@@ -23,40 +24,101 @@ def load_ISIC_tensors(data_dir: str, num_images: int, split: float, w: int = 300
     
     Returns
     -------
-        A tuple of two tuples: ((X_train, Y_train), (X_test, Y_test))
+        A tuple of tensors: X_train, Y_train, X_test, Y_test, positive_anchors, negative_anchors
     """
+
+    config = json.load(open("utility/config.json", "r"))
+    data_dir = config["data_dir"]
+    class_split = config["class_split"]
+    max_images = config["max_images"]
+    anchor_count = config["anchor_count"]
+    w = config["image_width"]
+    h = config["image_height"]
+    train_split = config["train_split"]
+
     real_image_names  = list_ISIC_images(data_dir + "/images")
     metadata = pd.read_csv(data_dir + "/ISIC_2020_Training_GroundTruth.csv")
     image_names = metadata['image_name'].values
+
     image_names = list(set(real_image_names).intersection(set([name for name in image_names])))
     metadata = metadata[metadata['image_name'].isin(image_names)]
-    np.random.shuffle(image_names)
 
-    X, Y = [], []
+    #get anchor positive and negatives
+    positive_names = metadata[metadata['target'] == 1]['image_name'].values
+    negative_names = metadata[metadata['target'] == 0]['image_name'].values
 
-    TrainX, TrainY, TestX, TestY = [], [], [], []
-    i = 0
-    print("Loading data...")
-    while True:
-        if (i == num_images) or (i == len(image_names)):
-            break
-        name = image_names[i]
-        i += 1
+    positives, negatives = [], []
 
-        if (i / num_images) * 100 % 10 == 0:
-            print(f"{(i / num_images) * 100:.1f}% done")
+    for i in range(anchor_count):
+        index1 = np.random.randint(0, len(positive_names))
+        index2 = np.random.randint(0, len(negative_names))
 
-        xb = tf.io.read_file(os.path.join(data_dir, "images/" + name + ".jpg"))
-        x  = tf.image.decode_jpeg(xb, channels=1)
+        xb = tf.io.read_file(os.path.join(data_dir, "images/" + positive_names[index1] + ".jpg"))
+        x  = tf.image.decode_jpeg(xb, channels=3)
         x  = tf.image.resize(x, [w, h], method=tf.image.ResizeMethod.BILINEAR)
         x  = tf.cast(x, tf.float32) / 255.0 - 0.5
 
-        y  = tf.cast(1 if metadata[metadata['image_name'] == name]["target"].values[0] == 1 else 0, tf.float32)
+        positives.append(x)
+
+        xb = tf.io.read_file(os.path.join(data_dir, "images/" + negative_names[index2] + ".jpg"))
+        x  = tf.image.decode_jpeg(xb, channels=3)
+        x  = tf.image.resize(x, [w, h], method=tf.image.ResizeMethod.BILINEAR)
+        x  = tf.cast(x, tf.float32) / 255.0 - 0.5
+
+        negatives.append(x)
+
+        # Remove the used names from the arrays to avoid duplicates
+        positive_names = np.delete(positive_names, index1)
+        negative_names = np.delete(negative_names, index2)
+
+    #ensure we dont use anchors for training        
+    metadata = metadata[metadata['image_name'].isin(positive_names) | metadata['image_name'].isin(negative_names)]
+    metadata = metadata.sample(frac=1)
+
+    # select the dataset
+    n_malignant = len(metadata[metadata['target'] == 1])
+    real_class_split = n_malignant / metadata.shape[0]
+    dataset = pd.DataFrame()
+    total_images = (1 / class_split) * n_malignant
+
+    if max_images < total_images:
+        dataset = metadata[metadata['target'] == 1].sample(n=int(max_images * class_split))
+        dataset = pd.concat([dataset, metadata[metadata['target'] == 0].sample(n=int(max_images * (1 - class_split)))])
+    else:
+        if class_split > real_class_split:
+            n_benign = total_images - n_malignant
+            dataset = pd.concat([metadata[metadata['target'] == 1], metadata[metadata['target'] == 0].sample(n=int(n_benign))])
+        elif class_split < real_class_split:
+            n_benign = len(metadata[metadata['target'] == 0])
+            n_malignant = (1 / class_split - 1) * n_benign
+            dataset = pd.concat([metadata[metadata['target'] == 0], metadata[metadata['target'] == 1].sample(n=int(n_malignant))])
+        else:
+            dataset = metadata
+
+    dataset = dataset.sample(frac=1)
+
+    print(f"Using {len(dataset)} images with {len(dataset[dataset['target'] == 1])} malignant and {len(dataset[dataset['target'] == 0])} benign")
+    # load the data
+    X, Y = [], []
+    TrainX, TrainY, TestX, TestY = [], [], [], []
+    print("Loading data...")
+    for i in range(len(dataset)):
+        name = dataset.iloc[i]['image_name']
+
+        if (i / len(dataset)) * 100 % 10 == 0:
+            print(f"{(i / len(dataset)) * 100:.1f}% done")
+
+        xb = tf.io.read_file(os.path.join(data_dir, "images/" + name + ".jpg"))
+        x  = tf.image.decode_jpeg(xb, channels=3)
+        x  = tf.image.resize(x, [w, h], method=tf.image.ResizeMethod.BILINEAR)
+        x  = tf.cast(x, tf.float32) / 255.0 - 0.5
+
+        y  = tf.cast(1 if dataset.iloc[i]['target'] == 1 else 0, tf.float32)
 
         X.append(x)
         Y.append(y)
     
-    num_train = int(len(X) * split)
+    num_train = int(len(X) * train_split)
     data_size = len(X)
     for i in range(data_size):
         index = np.random.randint(0, len(X))
@@ -68,15 +130,73 @@ def load_ISIC_tensors(data_dir: str, num_images: int, split: float, w: int = 300
             TestY.append(Y[index])
         X.pop(index)
         Y.pop(index)
-    
-    return (tf.stack(TrainX, 0), tf.stack(TrainY, 0)), (tf.stack(TestX, 0), tf.stack(TestY, 0))
 
-# if __name__ == "__main__":
-#     data_dir = "data"
-#     train_size = 3000
-#     (X_train, Y_train), (X_test, Y_test) = load_ISIC_tensors(data_dir, train_size, 0.8, 480, 360)
-#     print("stats: ")
-#     print(X_train.shape, Y_train.shape)
-#     print(X_test.shape, Y_test.shape)
-#     print("# benign: ", tf.reduce_sum(1 - Y_train).numpy(), tf.reduce_sum(1 - Y_test).numpy())
-#     print("# malignant: ", tf.reduce_sum(Y_train).numpy(), tf.reduce_sum(Y_test).numpy())
+    # Average all chosen tensors and normalize them
+    def normalize_tensor(tensor, mean, std):
+        return (tensor - mean) / (std + 1e-7)
+
+    TrainX = tf.stack(TrainX, 0)
+    TrainY = tf.stack(TrainY, 0)
+    TestX = tf.stack(TestX, 0)
+    TestY = tf.stack(TestY, 0)
+    positives = tf.stack(positives, 0)
+    negatives = tf.stack(negatives, 0)
+
+    # Compute the mean tensor of all images (train + test + anchors)
+    all_images = tf.concat([TrainX, TestX, positives, negatives], axis=0)
+    mean_image = tf.reduce_mean(all_images, axis=0)
+    std_image = tf.math.reduce_std(all_images, axis=0)
+
+    # Subtract mean and normalize each set
+    TrainX = normalize_tensor(TrainX, mean_image, std_image)
+    TestX = normalize_tensor(TestX, mean_image, std_image)
+    positives = normalize_tensor(positives, mean_image, std_image)
+    negatives = normalize_tensor(negatives, mean_image, std_image)
+
+    return tf.stack(TrainX, 0), \
+           tf.stack(TrainY, 0), \
+           tf.stack(TestX, 0), \
+           tf.stack(TestY, 0), \
+           tf.stack(positives, 0), \
+           tf.stack(negatives, 0)
+
+import matplotlib.pyplot as plt
+
+def plot_random_test_samples(X_test, Y_test, num_samples=8):
+    """
+    Plots a random sample of test images with their labels.
+
+    Parameters
+    ----------
+    X_test : tf.Tensor
+        Test images tensor.
+    Y_test : tf.Tensor
+        Test labels tensor.
+    num_samples : int
+        Number of samples to plot.
+    """
+    idxs = np.random.choice(X_test.shape[0], num_samples, replace=False)
+    images = X_test.numpy()[idxs]
+    labels = Y_test.numpy()[idxs]
+
+    plt.figure(figsize=(16, 2))
+    for i in range(num_samples):
+        plt.subplot(1, num_samples, i + 1)
+        img = images[i]
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title("Malignant" if labels[i] == 1 else "Benign")
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    data_dir = "data"
+    train_size = 3000
+    X_train, Y_train, X_test, Y_test, Panchors, Nanchors = load_ISIC_tensors()
+    print("stats: ")
+    print(X_train.shape, Y_train.shape)
+    print(X_test.shape, Y_test.shape)
+    print("# benign: ", tf.reduce_sum(1 - Y_train).numpy(), tf.reduce_sum(1 - Y_test).numpy())
+    print("# malignant: ", tf.reduce_sum(Y_train).numpy(), tf.reduce_sum(Y_test).numpy())
+
+    # plot_random_test_samples(X_test, Y_test, 20)
